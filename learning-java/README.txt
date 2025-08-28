48b36a840548c549845919713cc4b382bb18c7e4
aswile <aswile101@gmail.com>
2025-08-28 07:49:21 -0500

    added ways to add to planner

--- learning-java/README.txt
+++ learning-java/README.txt
@@ -1,1 +1,8 @@
-"xx" = no plans
+Daily planner ** 24H system"
+*format*, ** -#- indicates line can be read
+"00000000" unique identifire date + number of entries -00- day -00- month -0000- number of entries
+"xx"/ "00" -xx- empty no plans to view -00- if plans are present
+#"00/00/0000" date
+#"Monda" day of the week
+#"'00:00'" "data" time stamp followed by data
+" " " to indicate end of entry
--- learning-java/practice.java
+++ learning-java/practice.java
@@ -18,14 +18,85 @@ public class practice {
                     System.out.println("No plans today");
                     break;
                 }
-                System.out.println(line);
+                if(line.endsWith("#")){
+                    System.out.println(line);
+                }
             }
             read.close();
         
         } catch(FileNotFoundException e){
             System.out.println("FILE NOT FOUND");
         }
-        
-        
+    }
+
+    public static void addtoplanner(){
+        try{
+            FileWriter file = new FileWriter("todolist.txt");
+            Scanner input = new Scanner(System.in);
+            String day = "", data = "";
+            int hours = 0, minutes = 0;
+            Boolean correctdata = false;
+
+            while (!correctdata) {
+                System.out.print("Day of the weak: ");
+                day += input.nextLine();
+                switch (day.toLowerCase().strip()) {
+                    case "monday":
+                        file.write("Monday\n");
+                        correctdata = true;
+                        break;
+                    case "tuesday":
+                        file.write("Tuesday\n");
+                        correctdata = true;
+                        break;
+                    case "wednesday":
+                        file.write("Wednesday\n");
+                        correctdata = true;
+                        break;
+                    case "thursday":
+                        file.write("Thursday\n");
+                        correctdata = true;
+                        break;
+                    case "friday":
+                        file.write("Friday\n");
+                        correctdata = true;
+                        break;
+                    case "saturday":
+                        file.write("Saturday\n");
+                        correctdata = true;
+                        break;
+                    case "sunday":
+                        file.write("Sunday\n");
+                        correctdata = true;
+                        break;
+                    default:
+                        System.out.println("invalid information. Try again");
+                    
+                }
+            }
+            
+            System.out.println("\nTime:-");
+            System.out.print("Hour(24HR system): ");
+            hours = input.nextInt();
+            while (hours < 0 || hours > 23) {
+                System.out.println("invalid input");
+                hours = input.nextInt();
+            }
+            
+            System.out.print("minutes: ");
+            minutes = input.nextInt();
+            while (minutes < 0 || minutes > 59) {
+                System.out.println("invalid input");
+                minutes = input.nextInt();
+            }
+
+            data += String.format("%02d", hours);
+            data += ":" + String.format("%02d", minutes);
+            file.write(data);
+            file.close();
+            input.close();
+        }catch(IOException e) {
+            System.out.println("An error occurred.");
+        }
     }
 }
