b2415f9dbf7f38e4bb5d96fb7975ead510734e54
aswile <aswile101@gmail.com>
2025-09-18 17:32:14 -0500

    <upgrade>(all files) changes made to work with json instead of txt

--- src/main/java/com/example/learningjava/days.java
+++ src/main/java/com/example/learningjava/days.java
@@ -1,168 +1,66 @@
-package com.example.java.learningjava;
-import java.io.*;
+package com.example.learningjava;
 import java.util.*;
 
 public class days {
-    private String availability;
-    public String date;
-    public String time;
-    public String day = "?????";
-    private String data = "";
-    private planner plannerfile = new planner();
-    private ArrayList<String> todoList = plannerfile.todolist;
-
-    public days(String date){
-        int index = checkifpresent(date);
-            if(index >= 0){
-                    this.date = date;
-                    this.day = todoList.get(index + 2);
-                    while (!todoList.get(index).equals("\"")) {
-                        if (todoList.get(index).startsWith("*")){
-                            this.data += todoList.get(index).substring(2,todoList.get(index).length()) + "\n";
-                        }
-                        index++;
-                    }
-            }else{
-                    this.date = date;
-                    this.data = "??:??     \tNo entry present.";
-                }
-    }
+    private boolean availability;
+    private String date;
+    private String day;
+    private List<String> time;
+    private List<String> data;
 
     public days(){
        
     }
 
-    public String availability_get(){
-        return this.availability;
+    public String getdate(){
+        return date;
     }
 
-    public  void availability_set(String availability){
+    public  void setdate(String date){
+        this.date = date;
+    }
+    
+    public boolean isavailability(){
+        return availability;
+    }
+
+    public  void setavailability(boolean availability){
         this.availability = availability;
     }
 
-    public String data_get(){
-        return this.data;
+    public String getday(){
+        return day;
     }
 
-    public  void data_set(String data){
-        this.data = data;
+    public  void setday(String day){
+        this.day = day;
     }
 
-    private int checkifpresent(String input){
-        for (int i = 0; i < todoList.size(); i++) {
-            if (todoList.get(i).equals(input)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public void addtoplanner(){
-        adddate();
-        if(checkifpresent(date) < 0){
-        adddata();
-        todoList.remove(todoList.size()-1);
-        todoList.add(this.date);
-        todoList.add("00");
-        todoList.add(this.day);
-        todoList.add( "* " + this.data);
-        todoList.add("\"");
-        todoList.add("**END**");
-        plannerfile.addtoplanner();
-        }else{
-            System.out.println("Entry already present.");
-        }
-    }
-
-    public void adddate(){
-        boolean correctday = false; 
-        Scanner input = new Scanner(System.in);
-        while (!correctday) {
-            System.out.print("\nwhat is the day of the weak.\n\tA: Monday\tB: Tuesday\n\tC: Wednesday\tD: Thursday\n\tE: Friday\tF: Saturday\n\tG: Sunday \n\t:- ");
-            String dayofweek = input.nextLine();  
-            switch (dayofweek.toUpperCase()) {
-                case "A":
-                    this.day = "Monday";
-                    correctday = true;
-                    break;
-
-                case "B":
-                    this.day = "Tuesday";
-                    correctday = true;
-                    break;
-
-                case "C":
-                    this.day = "Wednesday";
-                    correctday = true;
-                    break;
-            
-                case "D":
-                    this.day = "Thursday";
-                    correctday = true;
-                    break;
-            
-                case "E":
-                    this.day = "Friday";
-                    correctday = true;
-                    break;
-            
-                case "F":
-                    this.day = "Saturday";
-                    correctday = true;
-                    break;
-
-                case "G":
-                    this.day = "Sunday";
-                    correctday = true;
-                    break;
-
-                default:
-                    break;
-          } }
-        
-        int day = 0, month = 0, year = 0;
-        while(day < 1 ||  day > 31 || month < 1 || month > 12 || year < 2025){
-            System.out.println("\nWhat is the date:-");
-            System.out.print("\tday: ");
-            day = input.nextInt();
-            System.out.print("\tmonth: ");
-            month = input.nextInt();
-            System.out.print("\tyear: ");
-            year = input.nextInt();
-            this.date = String.format("%02d/%02d/%04d", month,  day, year);
-            
-        }
-    }
-
-    public String addtime(){
-        Scanner input = new Scanner(System.in);
-        System.out.println("\nwhat is the time(24Hr):- ");
-        int hours = -1, minutes = -1;
-        while(hours < 0 || hours > 23 || minutes < 0 || minutes > 59){
-            System.out.print("\tHours:  ");
-            hours = input.nextInt();
-            System.out.print("\tminutes: ");
-            minutes = input.nextInt();
-        }
-        String time = String.format("%02d:%02d", hours, minutes);  
-        this.time = time;
+    public List<String> gettime(){
         return time;
     }
 
-    public void adddata(){
-        Scanner input = new Scanner(System.in);
-        System.out.print("\nEnter data:- \n\t- ");
-        String data = input.nextLine();
-        this.data = "* " + addtime() + "-\t" + data;
+    public  void setTime(List<String> time){
+        this.time = time;
     }
 
-   /* public void delete_data(String date){
+    public List<String> getData(){
+        return data;
+    }
+
+    public  void setData(List<String> data){
+        this.data = data;
+    }
+
+    /*
+
+    public void delete_data(String date){
         if(checkifpresent(date) > 0){
             for (int i = todoList.indexOf(date)+ No_OF_ELEMENTS; i > todoList.indexOf(date); i--) {
                 todoList.remove(i);    
             }
         }
-    }*/
+    }
 
     //add validation for choice options
     public void edit(String date){
@@ -210,13 +108,8 @@ public class days {
             }
         }
     }
-    }
+    }*/
 
-    public void to_String(){
-        System.out.println("\n\t** DAY PLANNER **");
-        System.out.printf("%-10s\t%-10s\n", date, day);
-        System.out.println(data_get());
-        
-    }
+    
     
 }
--- src/main/java/com/example/learningjava/planner.java
+++ src/main/java/com/example/learningjava/planner.java
@@ -1,39 +1,65 @@
-package com.example.java.learningjava;
+package com.example.learningjava;
 import java.io.*;
+import java.nio.file.*;
 import java.util.*;
+import java.time.*;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class planner {
-    public ArrayList<String> todolist = new ArrayList<>();
-    private String filename = "C:\\Users\\DELL\\Desktop\\demo\\src\\main\\java\\com\\example\\java\\todolist.txt";
+    public days day;
+    public List<days> todolist;
+    private String filename = "C:\\Users\\DELL\\Desktop\\demo\\src\\main\\java\\com\\example\\todolist.json";
+    private String content;
+    public String date;
+    ObjectMapper mapper;
 
-    public planner(){
+    public planner(String date){
         try{
-            BufferedReader reader = new BufferedReader(new FileReader(filename));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                todolist.add(line);
-            }
-            reader.close();
-        }catch(IOException e){
-            System.out.println("planner could not readfile");
+            this.content =new String(Files.readAllBytes(Paths.get(filename)));
+            mapper = new ObjectMapper();
+            todolist = mapper.readValue(content, new TypeReference<List<days>>(){});
+            this.date = date;
+            
+        }catch(Exception e){
+            System.out.println(e.toString());
         }
     }
 
-    public void addtoplanner(){
+    public void addtoplanner(days newDay){
+        todolist.add(newDay);
         try{
-            BufferedWriter writer = new BufferedWriter(new FileWriter(filename, false));
-            for (String l : todolist){
-                writer.write(l);
-                writer.newLine();
-            }
-            writer.close();
-        }catch(IOException e){
-            System.out.println("error: " + e.getMessage());
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(filename), todolist);
+        }catch(Exception e){
+            System.out.println(e.toString());
         }
+
     }
 
-    public ArrayList troubleshoot(){
-        return todolist;
+    public int checkifpresent(String querry){
+        for (int index = 0; index < todolist.size(); index++) {
+            if (todolist.get(index).getdate().equals(querry)) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public void to_String(String date){
+        System.out.println("\n\t** DAY PLANNER **");
+        int index = checkifpresent(date);
+        if (index > -1) {
+            System.out.println(date+" - "+ todolist.get(index).getday());
+            for (int i = 0; i < todolist.get(index).getData().size(); i++) {
+                System.out.println( todolist.get(index).gettime().get(i)+"\t"+ todolist.get(index).getData().get(i));
+            }  
+        }else{
+             System.out.println(date + " - No entries present");
+
+        }
+        
+        
     }
 
 
